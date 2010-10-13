d064d7f8de3d586affb3bac66b3a72746233f90e
Nathan Verni <npverni@gmail.com>
2010-10-13 15:44:10 -0400

    first pass

---
+++ template.rb
@@ -0,0 +1,349 @@
+# Application Generator Template
+# Modifies a Rails app to use RSpec, Cucumber, Factory Girl, MySQL and Devise...
+# Usage: rails new app2_name -d mysql -m /Users/npverni/apps/rails3-rumble-template/template.rb
+
+# More info: http://github.com/fortuity/rails3-mongoid-devise/
+
+# If you are customizing this template, you can use any methods provided by Thor::Actions
+# http://rdoc.info/rdoc/wycats/thor/blob/f939a3e8a854616784cac1dcff04ef4f3ee5f7ff/Thor/Actions.html
+# and Rails::Generators::Actions
+# http://github.com/rails/rails/blob/master/railties/lib/rails/generators/actions.rb
+
+puts "Modifying a new Rails app to use RSpec, Cucumber, Factory Girl, MySQL and Devise"
+
+#----------------------------------------------------------------------------
+# Configure
+#----------------------------------------------------------------------------
+
+if yes?('Would you like to use the Haml template system? (yes/no)')
+  haml_flag = true
+else
+  haml_flag = false
+end
+
+#----------------------------------------------------------------------------
+# Create the database
+#----------------------------------------------------------------------------
+puts "creating the database..."
+run 'rake db:create:all'
+
+#----------------------------------------------------------------------------
+# Set up git
+#----------------------------------------------------------------------------
+puts "setting up source control with 'git'..."
+# specific to Mac OS X
+
+append_file '.gitignore' do <<-FILE
+'.DS_Store'
+'.rvmrc'
+FILE
+end
+git :init
+git :add => '.'
+git :commit => "-m 'Initial commit of unmodified new Rails app'"
+
+#----------------------------------------------------------------------------
+# Remove the usual cruft
+#----------------------------------------------------------------------------
+puts "removing unneeded files..."
+run 'rm public/index.html'
+run 'rm public/favicon.ico'
+run 'rm public/images/rails.png'
+run 'rm README'
+run 'touch README'
+
+puts "banning spiders from your site by changing robots.txt..."
+gsub_file 'public/robots.txt', /# User-Agent/, 'User-Agent'
+gsub_file 'public/robots.txt', /# Disallow/, 'Disallow'
+
+#----------------------------------------------------------------------------
+# Haml Option
+#----------------------------------------------------------------------------
+if haml_flag
+  puts "setting up Gemfile for Haml..."
+  append_file 'Gemfile', "\n# Bundle gems needed for Haml\n"
+  gem 'haml', '3.0.18'
+  gem 'haml-rails', '0.2', :group => :development
+  # the following gems are used to generate Devise views for Haml
+  gem 'hpricot', '0.8.2', :group => :development
+  gem 'ruby_parser', '2.0.5', :group => :development
+end
+
+#----------------------------------------------------------------------------
+# jQuery Option
+#----------------------------------------------------------------------------
+gem 'jquery-rails', '0.2.3'
+
+#----------------------------------------------------------------------------
+# Set up jQuery
+#----------------------------------------------------------------------------
+
+run 'rm public/javascripts/rails.js'
+puts "replacing Prototype with jQuery"
+# "--ui" enables optional jQuery UI
+run 'rails generate jquery:install --ui'
+
+#----------------------------------------------------------------------------
+# Set up Devise
+#----------------------------------------------------------------------------
+puts "setting up Gemfile for Devise..."
+append_file 'Gemfile', "\n# Bundle gem needed for Devise\n"
+gem 'devise', '1.1.3'
+
+puts "installing Devise gem (takes a few minutes!)..."
+run 'bundle install'
+
+puts "creating 'config/initializers/devise.rb' Devise configuration file..."
+run 'rails generate devise:install'
+run 'rails generate devise:views'
+
+puts "modifying environment configuration files for Devise..."
+gsub_file 'config/environments/development.rb', /# Don't care if the mailer can't send/, '### ActionMailer Config'
+gsub_file 'config/environments/development.rb', /config.action_mailer.raise_delivery_errors = false/ do
+<<-RUBY
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # A dummy setup for development - no deliveries, but logged
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+RUBY
+end
+gsub_file 'config/environments/production.rb', /config.i18n.fallbacks = true/ do
+<<-RUBY
+config.i18n.fallbacks = true
+
+  config.action_mailer.default_url_options = { :host => 'yourhost.com' }
+  ### ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+RUBY
+end
+
+puts "creating a User model and modifying routes for Devise..."
+run 'rails generate devise User'
+run 'rake db:migrate'
+
+
+
+
+#----------------------------------------------------------------------------
+# Create a home page
+#----------------------------------------------------------------------------
+puts "create a home controller and view"
+generate(:controller, "home index")
+gsub_file 'config/routes.rb', /get \"home\/index\"/, 'root :to => "home#index"'
+
+puts "set up a simple demonstration of Devise"
+gsub_file 'app/controllers/home_controller.rb', /def index/ do
+<<-RUBY
+def index
+    @users = User.all
+RUBY
+end
+
+if haml_flag
+  run 'rm app/views/home/index.html.haml'
+  # we have to use single-quote-style-heredoc to avoid interpolation
+  create_file 'app/views/home/index.html.haml' do 
+<<-'FILE'
+- @users.each do |user|
+  %p User: #{link_to user.email, user}
+FILE
+  end
+else
+  append_file 'app/views/home/index.html.erb' do <<-FILE
+<% @users.each do |user| %>
+  <p>User: <%=link_to user.email, user %></p>
+<% end %>
+  FILE
+  end
+end
+
+#----------------------------------------------------------------------------
+# Create a users page
+#----------------------------------------------------------------------------
+generate(:controller, "users show")
+gsub_file 'config/routes.rb', /get \"users\/show\"/, '#get \"users\/show\"'
+gsub_file 'config/routes.rb', /devise_for :users/ do
+<<-RUBY
+devise_for :users
+  resources :users, :only => :show
+RUBY
+end
+
+gsub_file 'app/controllers/users_controller.rb', /def show/ do
+<<-RUBY
+before_filter :authenticate_user!
+
+  def show
+    @user = User.find(params[:id])
+RUBY
+end
+
+if haml_flag
+  run 'rm app/views/users/show.html.haml'
+  # we have to use single-quote-style-heredoc to avoid interpolation
+  create_file 'app/views/users/show.html.haml' do <<-'FILE'
+%p
+  User: #{@user.email}
+  FILE
+  end
+else
+  append_file 'app/views/users/show.html.erb' do <<-FILE
+<p>User: <%= @user.email %></p>
+  FILE
+  end
+end
+
+if haml_flag
+  create_file "app/views/devise/menu/_login_items.html.haml" do <<-'FILE'
+- if user_signed_in?
+  %li
+    = link_to('Logout', destroy_user_session_path)
+- else
+  %li
+    = link_to('Login', new_user_session_path)
+  FILE
+  end
+else
+  create_file "app/views/devise/menu/_login_items.html.erb" do <<-FILE
+<% if user_signed_in? %>
+  <li>
+  <%= link_to('Logout', destroy_user_session_path) %>        
+  </li>
+<% else %>
+  <li>
+  <%= link_to('Login', new_user_session_path)  %>  
+  </li>
+<% end %>
+  FILE
+  end
+end
+
+if haml_flag
+  create_file "app/views/devise/menu/_registration_items.html.haml" do <<-'FILE'
+- if user_signed_in?
+  %li
+    = link_to('Edit account', edit_user_registration_path)
+- else
+  %li
+    = link_to('Sign up', new_user_registration_path)
+  FILE
+  end
+else
+  create_file "app/views/devise/menu/_registration_items.html.erb" do <<-FILE
+<% if user_signed_in? %>
+  <li>
+  <%= link_to('Edit account', edit_user_registration_path) %>
+  </li>
+<% else %>
+  <li>
+  <%= link_to('Sign up', new_user_registration_path)  %>
+  </li>
+<% end %>
+  FILE
+  end
+end
+
+#----------------------------------------------------------------------------
+# Generate Application Layout
+#----------------------------------------------------------------------------
+if haml_flag
+  run 'rm app/views/layouts/application.html.erb'
+  create_file 'app/views/layouts/application.html.haml' do <<-FILE
+!!!
+%html
+  %head
+    %title Testapp
+    = stylesheet_link_tag :all
+    = javascript_include_tag :defaults
+    = csrf_meta_tag
+  %body
+    %ul.hmenu
+      = render 'devise/menu/registration_items'
+      = render 'devise/menu/login_items'
+    %p{:style => "color: green"}= notice
+    %p{:style => "color: red"}= alert
+    = yield
+FILE
+  end
+else
+  inject_into_file 'app/views/layouts/application.html.erb', :after => "<body>\n" do
+  <<-RUBY
+<ul class="hmenu">
+  <%= render 'devise/menu/registration_items' %>
+  <%= render 'devise/menu/login_items' %>
+</ul>
+<p style="color: green"><%= notice %></p>
+<p style="color: red"><%= alert %></p>
+RUBY
+  end
+end
+
+#----------------------------------------------------------------------------
+# Add Stylesheets
+#----------------------------------------------------------------------------
+create_file 'public/stylesheets/application.css' do <<-FILE
+ul.hmenu {
+  list-style: none;	
+  margin: 0 0 2em;
+  padding: 0;
+}
+
+ul.hmenu li {
+  display: inline;  
+}
+FILE
+end
+
+#----------------------------------------------------------------------------
+# Create a default user
+#----------------------------------------------------------------------------
+puts "creating a default user"
+append_file 'db/seeds.rb' do <<-FILE
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :email => 'user@test.com', :password => 'please', :password_confirmation => 'please'
+puts 'New user created'
+FILE
+end
+run 'rake db:seed'
+
+#----------------------------------------------------------------------------
+# Setup RSpec & Cucumber
+#----------------------------------------------------------------------------
+puts 'Setting up RSpec and Cucumber'
+append_file 'Gemfile' do <<-FILE
+group :test do
+  gem "rspec"
+  gem "rspec-rails",      ">= 2.0.0.beta"
+  gem "machinist",        :git => "git://github.com/notahat/machinist.git"
+  gem "faker"
+  gem "ZenTest"
+  gem "autotest"
+  gem "autotest-rails"
+  gem "cucumber",         :git => "git://github.com/aslakhellesoy/cucumber.git"
+  gem "database_cleaner", :git => 'git://github.com/bmabey/database_cleaner.git'
+  gem "cucumber-rails",   :git => "git://github.com/aslakhellesoy/cucumber-rails.git"
+  gem "capybara"
+  gem "capybara-envjs"
+  gem "launchy"
+  gem "ruby-debug"
+end
+FILE
+end
+
+run 'bundle install'
+run 'rails generate rspec:install'
+run 'rails cucumber:skeleton --capybara --rspec'
+#----------------------------------------------------------------------------
+# Finish up
+#----------------------------------------------------------------------------
+puts "checking everything into git..."
+git :add => '.'
+git :commit => "-am 'initial setup'"
+
+puts "Done setting up your Rails app."
